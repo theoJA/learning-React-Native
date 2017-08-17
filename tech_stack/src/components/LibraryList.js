b17ad01e39b1f20db0e2e4c3906ee3e110cd1fea
theoJA <theophilus.ja@gmail.com>
2017-08-17 01:34:36 -0400

    Added <ListView />, life-cycle method, passed in each library in this.props.libraries as props for <ListItem />

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import { ListView } from 'react-native'
 import { connect } from 'react-redux'
+import ListItem from './ListItem'
 
 class LibraryList extends Component {
+
+  componentWillMount() {
+    // boilerplate code for initializing datasource for the ListView
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    })
+    // we're taking the datasource obj (ds) and telling it to take 
+    //  the list of libraries from this.props.libraries
+    // Just a fancy wrapper for data we want to display
+    this.dataSource = ds.cloneWithRows(this.props.libraries)
+  }
+  
+  renderRow(library) {
+    return <ListItem library={library} />
+  }
+
   render() {
-    console.log(this.props)
-    return
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    )
   }
 }
 
