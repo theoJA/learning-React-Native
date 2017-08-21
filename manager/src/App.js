cb1dc7fadb87408342cee177b3786a00cfeb5fab
theoJA <theophilus.ja@gmail.com>
2017-08-21 19:48:25 -0400

    Removed LoginForm and added Router component

--- manager/src/App.js
+++ manager/src/App.js
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import firebase from 'firebase'
 import ReduxThunk from 'redux-thunk'
-import LoginForm from './components/LoginForm'
 import reducers from './reducers'
+import Router from './Router'
 
 class App extends Component {
   
@@ -27,8 +27,8 @@ class App extends Component {
 
     return (
       <Provider store={store}>
-        <View>
-          <LoginForm />
+        <View style={{ flex: 1 }}>
+          <Router />
         </View>
       </Provider>
     )
