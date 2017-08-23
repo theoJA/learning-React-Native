e895ea1d7d5bfd652d15a559b4ce602606e81b84
theoJA <theophilus.ja@gmail.com>
2017-08-22 20:59:40 -0400

    Added new Scene tags and some styling

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,19 +1,50 @@
 import React from 'react'
-import { Scene, Router } from 'react-native-router-flux'
+import { Scene, Router, Actions } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
+import EmployeeList from './components/EmployeeList'
+import EmployeeCreate from './components/EmployeeCreate'
 
 const RouterComponent = () => {
+  const { navBarStyle, navBarTitleStyle } = styles
+
   return (
     <Router>
-      <Scene key="root">
-        <Scene 
-          key="login"
-          component={LoginForm}
-          title="Please Login"
-        />
+      <Scene key="root" navigationBarStyle={navBarStyle} titleStyle={navBarTitleStyle} hideNavBar>
+        <Scene key="auth">
+          <Scene
+            key="login"
+            component={LoginForm}
+            title="Please Login"
+          />
+        </Scene>
+        
+        <Scene key="main">
+          <Scene 
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+            rightTitle=" Add"
+            onRight={() => Actions.employeeCreate()}
+            initial
+          />
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
+          />
+        </Scene>
       </Scene>
     </Router>
   )
+}
+
+const styles = {
+  navBarStyle: {
+    backgroundColor: '#AED6F1'
+  },
+  navBarTitleStyle: {
+    alignSelf: 'center'
+  }
 }
 
 export default RouterComponent
