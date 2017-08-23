c99e25505a2b4a4acaa1a9537cbc96a816f77afe
theoJA <theophilus.ja@gmail.com>
2017-08-22 21:00:37 -0400

    Added Actions.main()

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+import { Actions } from 'react-native-router-flux'
 import { 
   EMAIL_CHANGED, 
   PASSWORD_CHANGED,
@@ -43,6 +44,9 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS, 
     payload: user 
   })
+
+  // employeeList refers to the Scene's key!
+  Actions.main()
 }
 
 const loginUserFail = (dispatch) => {
