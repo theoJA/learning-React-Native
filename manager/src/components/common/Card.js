4221ca80ddb741563e1b1f3b6e22550ccfec8edf
theoJA <theophilus.ja@gmail.com>
2017-08-22 21:01:36 -0400

    Changed background color

--- manager/src/components/common/Card.js
+++ manager/src/components/common/Card.js
@@ -14,6 +14,7 @@ const styles = {
    borderWidth: 1,
    borderRadius: 2,
    borderColor: '#ddd',
+   //backgroundColor: '#F9E79F',
    borderBottomWidth: 0,
    shadowColor: '#000',
    shadowOffset: { width: 0, height: 2 },
