97d3a4bfa332891904bc4b72ccca632275164384
theoJA <theophilus.ja@gmail.com>
2017-08-28 23:36:41 -0400

    Added action creator to fetch employees from firebase

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase'
 import { Actions } from 'react-native-router-flux'
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types.js'
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -29,5 +30,16 @@ export const employeeCreate = ({ name, phone, shift }) => {
         })
         Actions.pop()
       })  // pop() is for moving the scene back to the previous scene  
+  }
+}
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth()
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {  // snapshot is NOT the array of emps. Its an obj that describes the data that is sitting in the bucket of data
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() })
+      })
   }
 }
