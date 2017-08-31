efd0973d166f2478d33bfe2c144bbb92b4882853
theoJA <theophilus.ja@gmail.com>
2017-08-30 23:38:50 -0400

    Added onPress methods to each employee

--- manager/src/components/ListItem.js
+++ manager/src/components/ListItem.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
-import { Text }  from 'react-native'
+import { Text, TouchableWithoutFeedback, View }  from 'react-native'
+import { Actions } from 'react-native-router-flux'
 import { CardSection } from './common'
 
 class ListItem extends Component {
+  
+  onRowPress() {
+    Actions.employeeEdit({ employee: this.props.employee })
+    // switching to the employeeCreate scene 
+    //  and passing on the prop employee to the scene
+  }
+  
   render() {
     const { name } = this.props.employee
 
     return (
-      <CardSection>
-        <Text style={styles.titleStyle}>
-          {name}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>
+              {name}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
